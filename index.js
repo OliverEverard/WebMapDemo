8f07450eb15b54a30e0c7f9197bc8ceba679ff68
Oliver Everard <53011298+OliverEverard@users.noreply.github.com>
2023-03-03 15:11:58 +0000

    Simplify

--- index.js
+++ index.js
@@ -1,41 +1,14 @@
-var bngBoundariesStyle = {
-  "color": "#ff7800",
-  "weight": 5,
-  "opacity": 0.65
-};
-
 var localPlanningAuthStyle = {
   "color": "#3297a8",
   "weight": 5,
   "opacity": 0.65
 };
 
-var countyBoundariesStyle = {
-  "color": "#45008f",
-  "weight": 5,
-  "opacity": 0.65
-};
-
-var nationalCharacterAreasStyle = {
-  "color": "#268f00",
-  "weight": 5,
-  "opacity": 0.65
-};
-
-
 var map = L.map('map').setView([51.505, -0.09], 13);
 
 L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
   maxZoom: 18,
 }).addTo(map);
-
-var bngBoundaries = L.geoJSON(null, {
-  style: bngBoundariesStyle,
-  onEachFeature: function(feature, layer) {
-    var label = feature.properties['Site Name'];
-    layer.bindTooltip(label);
-  }
-});
 
 var localPlanningAuthoritiesGeoJSON = L.geoJSON(null, {
   style: localPlanningAuthStyle,
@@ -45,32 +18,9 @@ var localPlanningAuthoritiesGeoJSON = L.geoJSON(null, {
   }
   });
   
-  var nationalCharacterAreasGeoJSON = L.geoJSON(null, {
-    style: nationalCharacterAreasStyle,
-    });
-
-    var countyBoundariesGeoJSON = L.geoJSON(null, {
-      style: countyBoundariesStyle,
-      onEachFeature: function(feature, layer) {
-        var label = feature.properties['CTYUA22NM'];
-        layer.bindTooltip(label);
-      }
-      });
-
-$.getJSON("./src/Biobank-BNG.geojson", function(data) {
-  bngBoundaries.addData(data);
-});
 
 $.getJSON("./src/localPlanningAuthorities.geojson", function(data) {
   localPlanningAuthoritiesGeoJSON.addData(data);
-});
-
-$.getJSON("./src/nationalCharacterAreas.geojson", function(data) {
-  nationalCharacterAreasGeoJSON.addData(data);
-});
-
-$.getJSON("./src/countyBoundaries.geojson", function(data) {
-  countyBoundariesGeoJSON.addData(data);
 });
 
 // Create a base layer
@@ -86,10 +36,7 @@ var baseLayer = {
 
 // Create a control object with the layer toggle
 var overlayMaps = {
-  "BNG Boundaries": bngBoundaries,
   "Local Planning Authorities": localPlanningAuthoritiesGeoJSON,
-  "National Character Areas": nationalCharacterAreasGeoJSON,
-  "County Boundaries": countyBoundariesGeoJSON,
 };
 
 L.control.layers(baseLayer, overlayMaps, {position: 'topright'}).addTo(map);
